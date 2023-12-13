b04fa387241c2a3066bea0b955d5544318577357
Nadeesha Cabral <n@nadeesha.me>
2023-12-13 16:55:01 +1100

    test: Fixing tests in ci

--- ts-core/src/tests/monolith/d.ts
+++ ts-core/src/tests/monolith/d.ts
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 import { Differential } from "../../Differential";
 
-process.env.DIFFERENTIAL_API_ENDPOINT_OVERRIDE = "http://0.0.0.0:4000";
+if (!process.env.DIFFERENTIAL_API_SECRET) {
+  throw new Error("Missing env DIFFERENTIAL_API_SECRET");
+}
 
-export const d = new Differential(
-  "sk_6299d50747b15b7fc223897bd1c5d84ef83ff369c926bd68d1ccd64285ba84f8"
-);
+export const d = new Differential(process.env.DIFFERENTIAL_API_SECRET);
