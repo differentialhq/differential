f1de61e714582980501764585f09b0968a223938
Nadeesha Cabral <n@nadeesha.me>
2023-12-18 20:32:35 +1100

    fix: Move differential-app out of the monorepo

--- differential-app/src/d.ts
+++
@@ -1,7 +0,0 @@
-import { Differential } from "@differentialhq/core";
-
-// This is a temporary API sercret for testing purposes, created by npm init.
-// You can obtain your own API sercret by signing up at https://differential.dev
-const apiSecret = "REPLACE_ME";
-
-export const d = new Differential(apiSecret);
