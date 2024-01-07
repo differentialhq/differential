3652bac2ce78a5f0e4beae68d2cb5604d2c1af22
John Smith <john@johnjcsmith.com>
2024-01-08 06:49:22 +1030

    feat: Initial service metrics with influx DB (#43)
    Implements event storage and aggregation with
    [InfluxDB](https://github.com/influxdata/influxdb) time series database.
    
    - Introduces a `writeEvent` which will publish events using the
    [InfluxDB node
    client](https://github.com/influxdata/influxdb-client-js/) which will
    handle batching / background data writing
    - Add events tracking for `jobCreated`, `jobResulted`, `machinePing`
    - Creates some basic event aggregation queries for retrieving metrics
    - Adds a `getFunctionMetrics` endpoint for retrieving request count and
    average execution time for a given time period
    
    Please note: This PR should be safe to deploy _without_ provisioning any
    InfluxDB instance based on the
    [INFLUXDB_ENABLED](https://github.com/differentialhq/differential/blob/efea7b2eb62092c8591f53f81e2a1417514b7b34/control-plane/src/modules/influx.ts#L6)
    flag which will gate the feature.

---
+++ control-plane/src/modules/influx.ts
@@ -0,0 +1,29 @@
+import { InfluxDB } from '@influxdata/influxdb-client'
+import { invariant } from '../utils';
+
+// Temporary feature flag for influxdb
+let client: InfluxDB | undefined;
+if (process.env.INFLUXDB_ENABLED) {
+  const token = invariant(
+    process.env.INFLUXDB_TOKEN,
+    "INFLUXDB_TOKEN must be set"
+  );
+
+  const url = invariant(
+    process.env.INFLUXDB_URL,
+    "INFLUXDB_URL must be set"
+  );
+
+  client = new InfluxDB({url, token})
+} else {
+  console.warn("INFLUXDB_ENABLED is not set, metrics will not be sent to influxdb");
+}
+
+export const INFLUXDB_ORG = 'differential';
+export const INFLUXDB_BUCKET = 'differential';
+
+export const queryClient = client?.getQueryApi(INFLUXDB_ORG)
+export const writeClient = client?.getWriteApi(INFLUXDB_ORG, INFLUXDB_BUCKET , 'ms', {
+  ...(process.env['NODE_ENV'] !== 'production' ? { flushInterval: 1000 } : {})
+})
+
