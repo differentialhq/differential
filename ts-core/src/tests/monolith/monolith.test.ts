db017d677b5133496647bcefc471a06fac2fd447
Nadeesha Cabral <n@nadeesha.me>
2024-01-12 23:10:17 +1100

    fix: Improve polling and shutdown stability (#62)
    - Throw error when a service attempts to start again.
    - Scope polling clients to `PollingAgent` and establish
    `AbortController`s.
    - Refactor tests for stability

--- ts-core/src/tests/monolith/monolith.test.ts
+++ ts-core/src/tests/monolith/monolith.test.ts
@@ -4,6 +4,16 @@ import { expertService } from "./expert";
 import { facadeService } from "./facade";
 
 describe("monolith", () => {
+  beforeAll(async () => {
+    await expertService.start();
+    await facadeService.start();
+  });
+
+  afterAll(async () => {
+    await expertService.stop();
+    await facadeService.stop();
+  }, 10000);
+
   it("should not import a service, if we're just consuming it", async () => {
     const db = d.client<typeof dbService>("db", { background: true });
 
@@ -16,20 +26,14 @@ describe("monolith", () => {
   });
 
   it("should be able to call a service", async () => {
-    await expertService.start();
-
     const result = await d
       .client<typeof expertService>("expert")
       .callExpert("Can't touch this");
 
     expect(result).toBe("Expert says: Can't touch this");
-
-    await expertService.stop();
   }, 10000);
 
   it("service client should return the same result as 'call'", async () => {
-    await expertService.start();
-
     const result = await d.call<typeof expertService, "callExpert">(
       "expert",
       "callExpert",
@@ -40,14 +44,9 @@ describe("monolith", () => {
     const clientResult = await client.callExpert("Can't touch this");
 
     expect(clientResult).toBe(result);
-
-    await expertService.stop();
   }, 10000);
 
   it("should be able to call a service from another service", async () => {
-    await facadeService.start();
-    await expertService.start();
-
     const result = await d
       .client<typeof facadeService>("facade")
       .interFunctionCall({
@@ -68,15 +67,11 @@ describe("monolith", () => {
   "Expert says: foobar",
 ]
 `);
-
-    await facadeService.stop();
-    await expertService.stop();
   }, 10000);
 
-  it("should be ok to start the service idempotently", async () => {
-    await expertService.start();
-    await expertService.start();
-
-    await expertService.stop();
-  }, 20000);
+  it("should not let an already started service start again", async () => {
+    await expect(expertService.start()).rejects.toThrowError(
+      "Service is already started"
+    );
+  });
 });
