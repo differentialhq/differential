fb4d1a1172774bb704329d150f7bb3e68e671b59
Nadeesha Cabral <n@nadeesha.me>
2024-01-13 21:07:55 +1100

    chore: Run metrics tests on a real database (#70)
    Run metrics tests on a real database.

--- control-plane/src/modules/event-aggregation.ts
+++ control-plane/src/modules/event-aggregation.ts
@@ -1,19 +1,22 @@
-import { flux } from '@influxdata/influxdb-client'
-import { INFLUXDB_BUCKET, queryClient } from './influx'
+import { flux } from "@influxdata/influxdb-client";
+import { INFLUXDB_BUCKET, queryClient } from "./influx";
 
 type TimeRange = {
-  start: Date
-  stop: Date
-}
+  start: Date;
+  stop: Date;
+};
 
 type JobComposite = {
-  clusterId: string
-  serviceName: string
-  functionName: string 
-}
+  clusterId: string;
+  serviceName: string;
+  functionName: string;
+};
 
 // Build a flux query to get the average execution time for a given function over a given time range
-export const resultExecutionTimeQuery = (target: JobComposite, range: TimeRange) => flux`from(bucket: "${INFLUXDB_BUCKET}")
+export const resultExecutionTimeQuery = (
+  target: JobComposite,
+  range: TimeRange
+) => flux`from(bucket: "${INFLUXDB_BUCKET}")
   |> range(start: ${range.start}, stop: ${range.stop})
   |> filter(fn: (r) => r["_measurement"] == "jobResulted")
   |> filter(fn: (r) => r["clusterId"] == "${target.clusterId}")
@@ -22,10 +25,13 @@ export const resultExecutionTimeQuery = (target: JobComposite, range: TimeRange)
   |> filter(fn: (r) => r["resultType"] == "resolution" or r["resultType"] == "rejection")
   |> filter(fn: (r) => r["_field"] == "functionExecutionTime")
   |> mean()
-`
+`;
 
 // Build a flux query to get the total number of calls for a given function over a given time range
-export const resultCountQuery = (target: JobComposite, range: TimeRange) => flux`from(bucket: "${INFLUXDB_BUCKET}")
+export const resultCountQuery = (
+  target: JobComposite,
+  range: TimeRange
+) => flux`from(bucket: "${INFLUXDB_BUCKET}")
   |> range(start: ${range.start}, stop: ${range.stop})
   |> filter(fn: (r) => r["_measurement"] == "jobResulted")
   |> filter(fn: (r) => r["clusterId"] == "${target.clusterId}")
@@ -34,7 +40,7 @@ export const resultCountQuery = (target: JobComposite, range: TimeRange) => flux
   |> filter(fn: (r) => r["resultType"] == "resolution" or r["resultType"] == "rejection")
   |> filter(fn: (r) => r["_field"] == "functionExecutionTime")
   |> count()
-`
+`;
 
 export const getFunctionMetrics = async (
   clusterId: string,
@@ -55,30 +61,38 @@ export const getFunctionMetrics = async (
   // Temporarily throw an error if the client is not initialized / enabled
   // QueryClient can be non-optinal once the influxdb flag is removed
   if (!queryClient) {
-    throw new Error("InfluxDB client not initialized. Metrics are not available.");
+    throw new Error(
+      "InfluxDB client not initialized. Metrics are not available."
+    );
   }
 
   // TODO: See if these can be typed better
-  const executionCount = await queryClient.collectRows(resultCountQuery({
-    clusterId,
-    serviceName,
-    functionName,
-  },
-    {
-      start,
-      stop,
-    }
-  ));
-  const executionTime = await queryClient.collectRows(resultExecutionTimeQuery({
-    clusterId,
-    serviceName,
-    functionName,
-  },
-    {
-      start,
-      stop,
-    }
-  ));
+  const executionCount = await queryClient.collectRows(
+    resultCountQuery(
+      {
+        clusterId,
+        serviceName,
+        functionName,
+      },
+      {
+        start,
+        stop,
+      }
+    )
+  );
+  const executionTime = await queryClient.collectRows(
+    resultExecutionTimeQuery(
+      {
+        clusterId,
+        serviceName,
+        functionName,
+      },
+      {
+        start,
+        stop,
+      }
+    )
+  );
 
   let metrics = {
     success: {
@@ -92,16 +106,18 @@ export const getFunctionMetrics = async (
   };
 
   // Build metrics object based on results by resultType
-  const processResults = (result: any, property: 'count' | 'avgExecutionTime'): void => {
-    if (result['resultType'] === "resolution") {
+  const processResults = (
+    result: any,
+    property: "count" | "avgExecutionTime"
+  ): void => {
+    if (result["resultType"] === "resolution") {
       metrics.success[property] = Math.round(result._value);
     } else if (result.resultType === "rejection") {
       metrics.failure[property] = Math.round(result._value);
     }
   };
-  executionCount.forEach((x: any) => processResults(x, 'count'));
-  executionTime.forEach((x: any) => processResults(x, 'avgExecutionTime'));
-
+  executionCount.forEach((x: any) => processResults(x, "count"));
+  executionTime.forEach((x: any) => processResults(x, "avgExecutionTime"));
 
   return metrics;
-}
+};
