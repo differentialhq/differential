bfb0628c13ef98f2c351bea8d73198e101f88762
Nadeesha Cabral <n@nadeesha.me>
2024-02-11 12:02:34 +1100

    fix: Adding advisory locaks to cron

--- control-plane/src/modules/cron.ts
+++ control-plane/src/modules/cron.ts
@@ -1,12 +1,33 @@
 // a naive cron implementation which will consume from a CDC later
 
+import * as lock from "./lock";
+
 const intervals: NodeJS.Timeout[] = [];
 
 export const registerCron = async (
+  name: string,
   fn: () => Promise<unknown>,
   { interval }: { interval: number },
 ) => {
-  const intervalId = setInterval(fn, interval);
+  const intervalId = setInterval(async () => {
+    const mutex = lock.createMutex(name);
+
+    const unlock = await mutex.tryLock();
+
+    if (!unlock) {
+      // already running by another process. skip
+      return;
+    }
+
+    try {
+      await fn();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await unlock();
+    }
+  }, interval);
+
   intervals.push(intervalId);
 };
 
--- control-plane/src/modules/jobs/jobs.ts
+++ control-plane/src/modules/jobs/jobs.ts
@@ -133,4 +133,4 @@ const storeMachineInfoBG = backgrounded(async function storeMachineInfo(
 });
 
 export const start = () =>
-  cron.registerCron(selfHealJobs, { interval: 1000 * 5 }); // 5 seconds
+  cron.registerCron("selfHealJobs", selfHealJobs, { interval: 1000 * 5 }); // 5 seconds
