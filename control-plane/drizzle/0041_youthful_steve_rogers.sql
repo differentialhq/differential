abcc9fab0c3ac62d3c4f396beb53747488ad3f37
Nadeesha Cabral <n@nadeesha.me>
2024-02-19 07:30:10 +1100

    Remove comment

--- control-plane/drizzle/0041_youthful_steve_rogers.sql
+++ control-plane/drizzle/0041_youthful_steve_rogers.sql
@@ -3,20 +3,6 @@ DELETE FROM "events";
 ALTER TABLE "events" DROP CONSTRAINT IF EXISTS "events_machine_id_machines_id_fk";
 
 --> statement-breakpoint
-/* 
-    Unfortunately in current drizzle-kit version we can't automatically get name for primary key.
-    We are working on making it available!
-
-    Meanwhile you can:
-        1. Check pk name in your database, by running
-            SELECT constraint_name FROM information_schema.table_constraints
-            WHERE table_schema = 'public'
-                AND table_name = 'machines'
-                AND constraint_type = 'PRIMARY KEY';
-        2. Uncomment code below and paste pk name manually
-        
-    Hope to release this update as soon as possible
-*/
 
 ALTER TABLE "machines" DROP CONSTRAINT IF EXISTS "machines_pkey" CASCADE;--> statement-breakpoint
 DO $$ BEGIN
