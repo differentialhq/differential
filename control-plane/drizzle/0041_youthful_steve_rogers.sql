8b36b3846c77f39d0f39295cf19aa5e33e029f45
Nadeesha Cabral <n@nadeesha.me>
2024-02-20 07:13:37 +1100

    Fix migration

--- control-plane/drizzle/0041_youthful_steve_rogers.sql
+++ control-plane/drizzle/0041_youthful_steve_rogers.sql
@@ -4,12 +4,17 @@ ALTER TABLE "events" DROP CONSTRAINT IF EXISTS "events_machine_id_machines_id_fk
 
 --> statement-breakpoint
 
+ALTER TABLE "machines" ADD CONSTRAINT "machines_id_cluster_id" PRIMARY KEY("id","cluster_id");
+
+--> statement-breakpoint
+
 ALTER TABLE "machines" DROP CONSTRAINT IF EXISTS "machines_pkey" CASCADE;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "events" ADD CONSTRAINT "events_machine_id_cluster_id_machines_id_cluster_id_fk" FOREIGN KEY ("machine_id","cluster_id") REFERENCES "machines"("id","cluster_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
+
 --> statement-breakpoint
+
 ALTER TABLE "machines" DROP CONSTRAINT IF EXISTS "machines_id_cluster_id" CASCADE;
-ALTER TABLE "machines" ADD CONSTRAINT "machines_id_cluster_id" PRIMARY KEY("id","cluster_id");
