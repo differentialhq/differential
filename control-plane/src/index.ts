27ae7cd997e594ee49ab158f555746d292f4dbf9
Nadeesha Cabral <n@nadeesha.me>
2024-03-08 08:54:57 +1100

    Adding metrics

--- control-plane/src/index.ts
+++ control-plane/src/index.ts
@@ -5,11 +5,23 @@ import cors from "@fastify/cors";
 import { initServer } from "@ts-rest/fastify";
 import fastify from "fastify";
 import process from "process";
+import { Counter, Summary, collectDefaultMetrics, register } from "prom-client";
+import * as deploymentScheduler from "./modules/deployment/scheduler";
 import * as jobs from "./modules/jobs/jobs";
 import * as events from "./modules/observability/events";
 import * as router from "./modules/router";
-import * as deploymentScheduler from "./modules/deployment/scheduler";
-import { collectDefaultMetrics, register } from "prom-client";
+
+export const httpDurations = new Summary({
+  name: "differential_http_operations_duration_ms",
+  help: "Duration of http operations in milliseconds",
+  labelNames: ["method", "route", "status"],
+});
+
+export const timeoutErrors = new Counter({
+  name: "differential_timeout_errors",
+  help: "Number of timeout errors",
+  labelNames: ["method", "route"],
+});
 
 const app = fastify({
   logger: process.env.ENABLE_FASTIFY_LOGGER === "true",
@@ -34,6 +46,23 @@ app.setErrorHandler((error, request, reply) => {
   return reply.status(500).send();
 });
 
+app.addHook("onResponse", (request, reply, done) => {
+  const route = request.routerPath;
+
+  httpDurations
+    .labels(request.method, route, reply.statusCode.toString())
+    .observe(reply.getResponseTime());
+
+  done();
+});
+
+app.addHook("onTimeout", (request, reply, done) => {
+  const route = request.routerPath;
+
+  timeoutErrors.labels(request.method, route).inc();
+  done();
+});
+
 const start = async () => {
   await jobs.start();
   await events.initialize();
@@ -45,8 +74,10 @@ const start = async () => {
 
   try {
     await app.listen({ port: 4000, host: "0.0.0.0" });
-    await metrics.listen({ port: 9091, host: "0.0.0.0" });
     console.log("Server listening on port 4000");
+
+    await metrics.listen({ port: 9091, host: "0.0.0.0" });
+    console.log("Metrics server listening on port 9091");
   } catch (err) {
     console.log(err);
     process.exit(1);
