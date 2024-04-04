0fa70cfbe5db3fc6e866f323ea7e68dfb15565b4
Nadeesha Cabral <n@nadeesha.me>
2024-04-04 23:04:24 +1100

    fix: Allow job exection with cluster tokens (#211)
    /cluster/:id/execute now works with cluster tokens as well as access
    point tokens.
    
    When cluster tokens are used, ACL for services = ["*"]

--- control-plane/src/modules/test/util.ts
+++ control-plane/src/modules/test/util.ts
@@ -3,13 +3,15 @@ import * as data from "../data";
 export const createOwner = async (params?: { clusterId?: string }) => {
   const clusterId = params?.clusterId || `test-cluster-${Math.random()}`;
 
+  const apiSecret = `test-secret-${Math.random()}`;
+
   await data.db
     .insert(data.clusters)
     .values({
       id: clusterId,
-      api_secret: `test-secret-${Math.random()}`,
+      api_secret: apiSecret,
     })
     .execute();
 
-  return { clusterId };
+  return { clusterId, apiSecret };
 };
